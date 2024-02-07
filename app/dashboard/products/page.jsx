0ee9eb2f286e842f310f4180096260832cb53685
Jorman Orrala <eduardorrala@live.com>
2024-02-07 16:25:31 -0500

    Add product page and user complete

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,8 +1,100 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Search from "@/app/ui/dashboard/search/search";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
 
 const ProductsPage = () => {
     return (
-        <div>ProductsPage</div>
+        <div className={styles.container}>
+            <div className={styles.top}>
+                <Search placeholder="Search for a product..." />
+                <Link href="/dashboard/products/add">
+                    <button className={styles.addButton}>Add New</button>
+                </Link>
+            </div>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <td>Title</td>
+                        <td>Description</td>
+                        <td>Price</td>
+                        <td>Created At</td>
+                        <td>Stock</td>
+                        <td>Action</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div className={styles.product}>
+                                <Image
+                                    src={"/noproduct.jpg"}
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                    className={styles.productImage}
+                                />
+                                username
+                            </div>
+                        </td>
+                        <td>description</td>
+                        <td>$25</td>
+                        <td>January</td>
+                        <td>20</td>
+                        <td>
+                            <div className={styles.buttons}>
+                                <Link href={`/dashboard/users/`}>
+                                    <button className={`${styles.button} ${styles.view}`}>
+                                        View
+                                    </button>
+                                </Link>
+                                <form >
+                                    <input type="hidden" name="id" />
+                                    <button className={`${styles.button} ${styles.delete}`}>
+                                        Delete
+                                    </button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div className={styles.product}>
+                                <Image
+                                    src={"/noproduct.jpg"}
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                    className={styles.productImage}
+                                />
+                                username
+                            </div>
+                        </td>
+                        <td>eduardorrala@live.com</td>
+                        <td>CreatedAt</td>
+                        <td>Admin</td>
+                        <td>Active</td>
+                        <td>
+                            <div className={styles.buttons}>
+                                <Link href={`/dashboard/users/`}>
+                                    <button className={`${styles.button} ${styles.view}`}>
+                                        View
+                                    </button>
+                                </Link>
+                                <form >
+                                    <input type="hidden" name="id" />
+                                    <button className={`${styles.button} ${styles.delete}`}>
+                                        Delete
+                                    </button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <Pagination count={2} />
+        </div>
     )
 }
 
--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,5 +1,6 @@
 import Search from "@/app/ui/dashboard/search/search";
 import styles from "@/app/ui/dashboard/users/users.module.css";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,6 +9,9 @@ const UsersPage = () => {
         <div className={styles.container}>
             <div className={styles.top}>
                 <Search placeholder="Search for a user..." />
+                <Link href="/dashboard/users/add">
+                    <button className={styles.addButton}>Add New</button>
+                </Link>
             </div>
             <table className={styles.table}>
                 <thead>
@@ -22,11 +26,55 @@ const UsersPage = () => {
                 </thead>
                 <tbody>
                     <tr>
-                        <td>Name data</td>
-                        <td>Email Data</td>
-                        <td>Created data</td>
-                        <td>Role data</td>
-                        <td>Status data</td>
+                        <td>
+                            <div className={styles.user}>
+                                <Image
+                                    src={"/noavatar.png"}
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                    className={styles.userImage}
+                                />
+                                username
+                            </div>
+                        </td>
+                        <td>eduardorrala@live.com</td>
+                        <td>CreatedAt</td>
+                        <td>Admin</td>
+                        <td>Active</td>
+                        <td>
+                            <div className={styles.buttons}>
+                                <Link href={`/dashboard/users/`}>
+                                    <button className={`${styles.button} ${styles.view}`}>
+                                        View
+                                    </button>
+                                </Link>
+                                <form >
+                                    <input type="hidden" name="id" />
+                                    <button className={`${styles.button} ${styles.delete}`}>
+                                        Delete
+                                    </button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div className={styles.user}>
+                                <Image
+                                    src={"/noavatar.png"}
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                    className={styles.userImage}
+                                />
+                                username
+                            </div>
+                        </td>
+                        <td>eduardorrala@live.com</td>
+                        <td>CreatedAt</td>
+                        <td>Admin</td>
+                        <td>Active</td>
                         <td>
                             <div className={styles.buttons}>
                                 <Link href={`/dashboard/users/`}>
@@ -45,6 +93,7 @@ const UsersPage = () => {
                     </tr>
                 </tbody>
             </table>
+            <Pagination count={2} />
         </div>
     )
 }
